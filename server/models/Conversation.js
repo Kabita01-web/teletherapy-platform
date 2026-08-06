c6aaadf1306f208243a78cec2b4c91ac2868401b
Kabita Bhurtel <kabitabhurtel222@gmail.com>
2026-08-06 22:14:30 +0545

    feat(server/models/Conversation): Add new schema for Conversation model, including participants and last message.

---
+++ server/models/Conversation.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    lastMessageAt: {
+      type: Date,
+      default: Date.now,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Ensure unique conversation between two users
+conversationSchema.index({ participants: 1 });
+
+module.exports = mongoose.model("Conversation", conversationSchema);
