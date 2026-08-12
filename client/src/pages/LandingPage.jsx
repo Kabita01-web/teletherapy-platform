9e7c8669acbb10acff8198578e14a0a69258e6ab
Kabita Bhurtel <kabitabhurtel222@gmail.com>
2026-08-12 20:57:04 +0545

    feat: Added 'Services' component to the landing page.

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,17 +1,14 @@
-import Navbar from '../components/landing/Navbar';
-import Hero from '../components/landing/Hero';
-import TrustFeatures from '../components/landing/TrustFeatures';
-import Services from '../components/landing/Services';
-import About from '../components/landing/About';
-import HowItWorks from '../components/landing/HowItWorks';
-import Testimonials from '../components/landing/Testimonials';
-import FinalCTA from '../components/landing/FinalCTA';
-import Footer from '../components/landing/Footer';
+import Hero from "../components/landing/Hero";
+import TrustFeatures from "../components/landing/TrustFeatures";
+import Services from "../components/landing/Services";
+import About from "../components/landing/About";
+import HowItWorks from "../components/landing/HowItWorks";
+import Testimonials from "../components/landing/Testimonials";
+import FinalCTA from "../components/landing/FinalCTA";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
       <main className="flex-grow">
         <Hero />
         <TrustFeatures />
@@ -21,7 +18,6 @@ export default function LandingPage() {
         <Testimonials />
         <FinalCTA />
       </main>
-      <Footer />
     </div>
   );
 }
